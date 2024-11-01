239a1a377c2d323fbd1e601ab10a74de4c494127
Leonardo Silva <leonardo.silva@supervisao.com.br>
2024-11-01 17:14:36 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const startDate = new Date('2023-01-08');
+
+document.getElementById('specialMessage').textContent = "Com amor! \u2764";
+
+function calculateDaysTogether(startDate) {
+    const currentDate = new Date();
+    const timeDifference = currentDate - startDate; 
+    const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    return daysDifference;
+}
+
+function updateDaysCounter() {
+    const daysCount = calculateDaysTogether(startDate);
+    document.getElementById('daysCount').textContent = daysCount;
+}
+
+function updateSpecialMessage(daysCount) {
+    const messageElement = document.getElementById('specialMessage');
+    if (daysCount) {
+        messageElement.innerHTML = `Fiz este contador de dias para sabermos quantos dias estamos juntos, e também para ver quais foram meus dias mais felizes da minha vida. Sou muito grato por tudo que você faz por mim, te amo agora e para sempre \u2764 <br><br> Te amo muito Dede pdf \u2764`;
+    }
+}
+
+// Inicializar
+document.addEventListener('DOMContentLoaded', () => {
+    const daysCount = calculateDaysTogether(startDate);
+    updateDaysCounter();
+    updateSpecialMessage(daysCount);
+});
